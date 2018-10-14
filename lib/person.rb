f3de024b4fd2aff1ee226192f4dbd2f117fa69c3
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-14 17:36:12 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 class Person
 
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size,
   :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(properties)
     properties.each { |key, value|
-      self.send(("#{key}="), value)
+      self.send("#{key}=", value)
     }
   end
 
